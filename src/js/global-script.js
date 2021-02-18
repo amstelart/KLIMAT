801994fc4486954806da681b675749077d53abe9
amstelart <amsteldroid@gmail.com>
2021-02-18 23:04:19 +0300

    + главная

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,6 +1,28 @@
 // Если на проекте jQuery
 $( document ).ready(function() {
   // code
+  $('.header-nav__toggler').click(function(event) {
+    $('.header-nav__toggler, .header-nav').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+  // fix top-menu
+  var shrinkHeader = 150;
+  var head = $('.page-header');
+  var heightHeader = head.height();
+  $(window).scroll(function() {
+    var scroll = $(this).scrollTop();
+    if ( scroll >= shrinkHeader ) {
+        // $('body').css('paddingTop',heightHeader);
+        head.addClass('shrink');
+      }
+      else {
+          // $('body').css('paddingTop',0);
+          head.removeClass('shrink');
+      }
+  });
+  // fix top-menu === end
+
   $('.product-box__carousel').slick({
     arrows:true,
     dots: true,
@@ -9,7 +31,34 @@ $( document ).ready(function() {
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
-    dotsClass: 'dots-style'
+    dotsClass: 'dots-style',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
   $('.brand__carousel').slick({
     arrows:false,
@@ -36,7 +85,7 @@ $( document ).ready(function() {
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
